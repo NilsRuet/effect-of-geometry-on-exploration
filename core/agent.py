fix eccentricity computation

core/agent.py
@@ -63,6 +63,7 @@ class Agent:
         best_action_index, losses, loss_per_space = self.policy.select(
             future_beliefs_per_space, valid_actions, idle_action_index
         )
+
         best_moves = actions_per_space[:, best_action_index]
         new_beliefs = future_beliefs_per_space[:, best_action_index]
 
@@ -116,20 +117,25 @@ class Agent:
             # compute distance and eccentricty for each action
             for action_i in range(len(actions)):
                 next_world_position = -world_translations[action_i]
-                eccentricity = abs(
-                    GeometryUtils.get_angle(
-                        next_world_position, current_world_position, obs
+                
+                if(np.linalg.norm(next_world_position - current_world_position) < 0.001):
+                    # In the idle case, there is no movement nor reorientation, so the observation kernel stays the same
+                    # ~= keeping the same orientation
+                    observation_kernels.append(self.spaces[space_i].beliefs.observation_kernel)
+                else:
+                    eccentricity = abs(
+                        GeometryUtils.get_angle(
+                            next_world_position, current_world_position, obs
+                        )
                     )
-                )
-                distance = np.linalg.norm(obs - next_world_position)
-                kernel = space.kernel_generator(eccentricity, distance)
-                observation_kernels.append(kernel)
+                    distance = np.linalg.norm(obs - next_world_position)
+                    kernel = space.kernel_generator(eccentricity, distance)
+                    observation_kernels.append(kernel)
 
             future_beliefs = b.propagate_actions(
                 actions, observation_kernels, f"space {space_i+1}/{space_count}: "
             )
             future_beliefs_per_space.append(future_beliefs)
-
         return np.array(future_beliefs_per_space)
 
     def get_current_frames(self):

core/simulation.py
@@ -20,8 +20,7 @@ from utils.rotationutils import RotationUtils
 
 class Simulation:
     def run(self, params: SimParams):
-
-        ## DEBUG
+        ### DEBUG
         # from debug.utils import draw_2D_function, draw_1D_function
         # def get_kernel_epsilon(x, y):
         #     vision_axis = (1, 1)
@@ -36,7 +35,6 @@ class Simulation:
         #         sample_space.distance_coef,
         #     )
         #     return generator(eccentricity, distance).epsilon
-        
         # draw_2D_function(get_kernel_epsilon, [[0, 5], [0, 5]])
         # draw_1D_function(lambda x: get_kernel_epsilon(np.sqrt(x), np.sqrt(x)), [0, 5])
 

main.py
@@ -12,23 +12,25 @@ from itertools import product
 
 
 def generate_exploration_params():
-    steps = 15
     all_params = []
+
+    # Fixed params
+    gamma = 0.5
+    steps = 15
     base_norm = 0.1
-
-    distance = 1
     initial_prior = 0.1
-
-    acuity_coef = 6
+    acuity_narrowness = 1.5
     distance_coef = 0.4
 
-    pos1 = np.array((-2, 1)) * distance
-    pos2 = np.array((0.25, 1)) * distance
+    pos1 = np.array((-1, 1))
+    pos2 = np.array((0.25, 1))
     positions = [pos1, pos2]
 
+    # Grid of variable params
     directions = [8]
     radius_limit = [1.4 * base_norm]
-    kernel_epsilons = [0.05, 0.1, 0.5, 1.0, 2.0]
+    kernel_epsilons = [0.1, 0.5, 1.0, 2.0, 4.0]
+    # kernel_epsilons = [4.0]
     merge_by_min_options = [True]
     idle_on_illegal_illegal_options = [True]
     options = list(
@@ -53,14 +55,14 @@ def generate_exploration_params():
                 target=pos,
                 markov_kernel_epsilon=kernel_epsilon,
                 initial_beliefs_covariance=initial_prior,
-                acuity_coef=acuity_coef,
+                acuity_coef=acuity_narrowness,
                 distance_coef=distance_coef
             )
             for pos in positions
         ]
         all_params.append(
             SimParams(
-                gamma=1,
+                gamma=gamma,
                 direction_count=direction_count,
                 belief_spaces=belief_space_params,
                 max_steps=steps,

view_trajectories.py
@@ -79,7 +79,8 @@ def plot_priors(simulation_dictionary, ax):
         states = step["states"]
         for i in range(space_count):
             cov_matrix = np.array(states[i]["beliefs_cov"])
-            volume = 2 * np.pi * np.sqrt(abs(cov_matrix[0][0])) * np.sqrt(abs(cov_matrix[1][1]))
+            # volume = 2 * np.pi * np.sqrt(abs(cov_matrix[0][0])) * np.sqrt(abs(cov_matrix[1][1]))
+            volume = cov_matrix[0][0] * cov_matrix[1][1] - cov_matrix[0][1] * cov_matrix[1][0]
             priors_cov[i].append(volume)
 
     if is_euclidean:
@@ -89,7 +90,7 @@ def plot_priors(simulation_dictionary, ax):
 
     # plot each loss
     for i, priors in enumerate(priors_cov):
-        ax.plot(priors, label=f"Space {i+1} value")
+        ax.plot(priors, label=f"Volume in space {i+1}")
 
     # Add labels and legend
     ax.set_xlabel("Time")
@@ -203,7 +204,7 @@ def _plot_traj(is_euclidean, targets, positions, ax):
             length_includes_head=True,
             color="gray",
         )
-        ax.text(*arrow[0], f"{i+1}", fontsize=7, color="red")
+        # ax.text(*arrow[0], f"{i+1}", fontsize=7, color="red")
 
     # Agent
     ax.scatter(
@@ -236,9 +237,9 @@ def plot_observation_kernels(simulation_dictionary, ax):
     is_euclidean = simulation_dictionary["params"]["gamma"] == 0
 
     if is_euclidean:
-        ax.set_title("Epistemic value (euclidean)")
+        ax.set_title("Kernel epsilon (euclidean)")
     else:
-        ax.set_title("Epistemic value (projective)")
+        ax.set_title("Kernel epsilon (projective)")
 
     # plot each loss
     for i, epsilon in enumerate(epsilon_history):